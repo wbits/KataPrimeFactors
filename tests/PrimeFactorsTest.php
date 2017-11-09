38788aa152123d0b5ae23f9e7a2fa62469d0991d
Dick <dick@insided.nl>
2017-11-09 10:33:47 +0100

    red, test two

--- tests/PrimeFactorsTest.php
+++ tests/PrimeFactorsTest.php
@@ -12,5 +12,10 @@ final class PrimeFactorsTest extends TestCase
     {
         self::assertEquals([], PrimeFactors::generate(1));
     }
+
+    public function testTwo()
+    {
+        self::assertEquals([2], PrimeFactors::generate(2));
+    }
 }
 
